f1cab53ead4dc38b4bf1b1aacbf5759d68981ef6
pattnaiktejas527-coder <pattnaiktejas527@gmail.com>
2026-06-01 13:10:25 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,27 @@
+#Find the square
+a=int(input("enter your no."))
+result=a*a
+print("square of the no is",result)
+
+#Check even or odd
+num=int(input("Enter your no."))
+if(num%2==0):
+    print("This is a even no.")
+else:
+    print("This is a odd no.")
+
+#Greater no
+p= int (input("Enter your no."))
+q= int (input("Enter your no."))
+if(p>q):
+    print("The greater no. is",p)
+else:
+    print("The greater no. is ",q)
+
+#Print no.
+for i in range(1,11):
+    print(i)
+
+#multiplication table
+for i in range(1,11):
+    print(i*5)
