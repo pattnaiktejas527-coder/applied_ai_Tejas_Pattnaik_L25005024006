600402bd981b01c3a52d9bde66eaeebd6e57de4b
pattnaiktejas527-coder <pattnaiktejas527@gmail.com>
2026-06-02 11:49:37 +0530

    Add files via upload

---
+++ Divisible.py
@@ -0,0 +1,8 @@
+#Divisible
+a=int(input("Enter a no. : "))
+if(a%5==0):
+    print("This no is divisible by 5")
+elif(a%11==0):
+    print("This no is divisible by 11")
+else:
+    print("This no is not divisible by 5 or 11 ")
