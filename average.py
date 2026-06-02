fd6468c897a26505c04a9554e478525963ea7649
pattnaiktejas527-coder <pattnaiktejas527@gmail.com>
2026-06-02 11:50:05 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+#average of 4 no.
+a=int(input("Enter 1st no. : "))
+b=int(input("Enter 2nd no. : "))
+c=int (input("Enter 3rd no. : "))
+d=int(input("Enter 4th no. : "))
+Avg=(a+b+c+d)/4
+print(Avg)
